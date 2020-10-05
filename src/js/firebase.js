055a6339f41637142e374e675e658be6be3a1421
Kiryll26 <60568848+Kiryll26@users.noreply.github.com>
2020-10-05 13:43:28 +0300

    Update firebase.js

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-	apiKey: "AIzaSyA20i4h6SbC6677tQZISptp09REHLXCkDM",
-	authDomain: "todofunction-59e93.firebaseapp.com",
-	databaseURL: "https://todofunction-59e93.firebaseio.com",
-	projectId: "todofunction-59e93",
-	storageBucket: "todofunction-59e93.appspot.com",
-	messagingSenderId: "97183160063",
-	appId: "1:97183160063:web:ab7bb02ec3d51c24dbdeca",
-	measurementId: "G-Y9NVRSYLDL"
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
+	measurementId: ""
 };
 import * as firebase from 'firebase';
 import database from "firebase/database";
